b41f0ad00b126c0b7c2df57e084f955b219b5af1
wwh <2792842511@qq.com>
2025-06-30 19:36:13 +0800

    字母组合

--- src/test/java/com/wwh/arithmetic/BackTracking.java
+++ src/test/java/com/wwh/arithmetic/BackTracking.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class BackTracking {
     
 }
+//回溯函数的常见参数：方法的传参、开始下标、当前路径、结果集等。
 class Solution1 {
     public List<List<Integer>> subsets(int[] nums) {
         // 回溯算法，返回所有子集
@@ -23,7 +24,7 @@ class Solution1 {
         }
     }
 }
-class Solution {
+class Solution2 {
     public List<List<Integer>> permute(int[] nums) {
         //给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
         List<List<Integer>> result = new ArrayList<>();
@@ -44,6 +45,35 @@ class Solution {
             Collections.swap(current, i, start);
             backtrack(current, result, n, start + 1);
             Collections.swap(current, i, start); // 撤销交换
+        }
+    }
+}
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+//给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result; // 如果输入为空，返回空列表
+        }
+        StringBuilder current = new StringBuilder(); // 用于存储当前组合
+        String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backtrack(digits, 0, current, result, mapping);
+        return result; // 返回所有组合
+    }
+    public void backtrack(String digits, int index, StringBuilder current, List<String> result, String[] mappStrings){
+        if ((index==digits.length())) {
+            result.add(current.toString()); // 如果当前组合长度等于输入长度，添加到结果
+            return;
+        }
+        int digit = digits.charAt(index) - '0'; // 获取当前数字
+        String letters = mappStrings[digit]; // 获取对应的字母
+        for(char letter : letters.toCharArray()){   
+            current.append(letter);
+            backtrack(digits, index + 1, current, result, mappStrings); // 递归调用
+            current.deleteCharAt(index); // 撤销选择?
+            
         }
     }
 }
