bc305e33c503ae067cc2ea792675625f3f1b5f06
wwh <2792842511@qq.com>
2025-07-15 19:10:11 +0800

    一道回溯

--- src/test/java/com/wwh/arithmetic/BackTracking.java
+++ src/test/java/com/wwh/arithmetic/BackTracking.java
@@ -51,7 +51,7 @@ class Solution2 {
 //给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
 
 //给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
-class Solution {
+class Solution3 {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
         if (digits == null || digits.length() == 0) {
@@ -74,6 +74,31 @@ class Solution {
             backtrack(digits, index + 1, current, result, mappStrings); // 递归调用
             current.deleteCharAt(index); // 撤销选择?
             
+        }
+    }
+}
+//给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target 
+//，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, current, result, 0);
+        return result;
+    }
+    private void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result, int sum) {
+        if (sum > target) {
+            return; // 如果当前组合的和大于目标，直接返回
+        }
+        if (sum == target) {
+            result.add(new ArrayList<>(current));
+            return; // 如果当前组合的和等于目标，添加到结果
+        }
+        for (int i = start; i < candidates.length; i++) {
+            current.add(candidates[i]); // 选择当前元素
+            backtrack(candidates, target, i, current, result, sum + candidates[i]); // 递归调用，允许重复使用当前元素
+            current.remove(current.size() - 1); // 撤销选择
         }
     }
 }
